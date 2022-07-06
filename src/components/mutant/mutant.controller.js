e85762cf085aa108446a2d0c881124fc4bd94510
gabejarano <ga.bejarano10@uniandes.edu.co>
2022-07-06 18:58:26 -0500

    create model mutant

--- src/components/mutant/mutant.controller.js
+++ src/components/mutant/mutant.controller.js
@@ -1,143 +1,22 @@
+const Mutant = require('./mutant.model');
 //Client dynamoDB
 const dynamoclient = require('../../services/dynamodb');
 const TABLE_NAME = "DNA_MUTANT";
 const { uuid } = require('uuidv4');
-const numberLetters = 4;
-const RIGTH = 'RTGTH';
-const DOWN = 'DOWN';
-const OBLIQUE_LEFT = 'OBLIQUE_LEFT';
-const OBLIQUE_RIGTH = 'OBLIQUE_RIGTH';
 
-var numberGenesMutants = 0;
-var currentx = 0;
-var currenty = 0;
-var moveRigth = true;
-var moveDown = true;
-var moveObliqueRigth = true;
-var moveObliqueLeft = true;
-var matrix = [[]];
-
-
-
-//Create a matrix of objects than contains the letter and its position in x and y.
-function createMatrix(dna) {
-    let matrix = [];
-    dna.forEach((gen, x) => {
-        var arreglo = gen.split('').map((gen, y) => { return { id: gen, x, y } })
-        matrix[x] = arreglo;
-    })
-    return matrix;
-}
-
-
-function validationNextLetter(gen, nextGen, numberLetter, direction) {
-    var validationDirection = direction === RIGTH ? moveRigth : direction === DOWN ? moveDown : direction === OBLIQUE_RIGTH ? moveObliqueRigth : moveObliqueLeft;
-    var x= direction === RIGTH ? gen.x : direction === DOWN ? gen.x + 1 : direction === OBLIQUE_RIGTH ? gen.x + 1 : gen.x - 1;
-    var y= direction === RIGTH ? gen.y + 1 : direction === DOWN ? gen.y : direction === OBLIQUE_RIGTH ? gen.y + 1 : gen.y + 1;
-    if (gen.id === nextGen.id) {
-        if (numberLetter === 2) {
-            numberGenesMutants++;
-            if (numberGenesMutants > 1) {
-                return true;
-            }
-            direction === RIGTH ? moveRigth = false : direction === DOWN ? moveDown = false : direction === OBLIQUE_RIGTH ? moveObliqueRigth = false : moveObliqueLeft = false;
-            return isMutant(currentx, currenty, numberLetters)
-        }
-        return isMutant(x, y, numberLetter - 1)
-    }
-    else if (validationDirection && gen.id !== nextGen.id) {
-        direction === RIGTH ? moveRigth = false : direction === DOWN ? moveDown = false : direction === OBLIQUE_RIGTH ? moveObliqueRigth = false : moveObliqueLeft = false;
-        return isMutant(currentx, currenty, numberLetters)
-    }
-}
-
-function setMoves() {
-    moveRigth = true;
-    moveDown = true;
-    moveObliqueRigth = true;
-    moveObliqueLefth = true;
-}
-
-function setVars(){
-    currentx = 0;
-    currenty = 0;
-    numberGenesMutants = 0;
-}
-
-function isMutant(x, y, numberLetter) {
-
-    try {
-        var gen = matrix[x][y];
-        var genRigth = matrix[x][y + 1];
-        var genDown = matrix[x + 1] ? matrix[x + 1][y] : undefined;
-        var genObliqueRigth = matrix[x + 1] ? matrix[x + 1][y + 1] : undefined;
-        var genObliqueLeft = matrix[x + 1] ? matrix[x + 1][y - 1] : undefined;
-
-        //Validation Rigth
-        if (gen.y + numberLetter <= matrix.length && moveRigth) {
-            return validationNextLetter(gen, genRigth, numberLetter, RIGTH)
-        } else {
-            moveRigth = false;
-        }
-        //Validation down
-        if (gen.x + numberLetter <= matrix.length && moveDown) {
-            return validationNextLetter(gen, genDown, numberLetter, DOWN)
-        } else {
-            moveDown = false;
-        }
-        //Validation obliqueRigth
-        if (gen.y + numberLetter <= matrix.length && gen.x + numberLetter <= matrix.length && moveObliqueRigth) {
-            return validationNextLetter(gen, genObliqueRigth, numberLetter, OBLIQUE_RIGTH)
-        } else {
-            moveObliqueRigth = false;
-        }
-        //Validation obliqueLeft
-        if (gen.y + numberLetter <= matrix.length && gen.x - numberLetter >= 0 && moveObliqueLeft) {
-            return validationNextLetter(gen, genObliqueLeft, numberLetter, OBLIQUE_LEFT)
-        } else {
-            moveObliqueLeft = false;
-        }
-
-        if (currenty + 1 < matrix.length) {
-            currenty++;
-            setMoves();
-            return isMutant(currentx, currenty, numberLetters)
-        }
-        else if (currentx + 1 < matrix.length) {
-            currenty = 0;
-            currentx++;
-            setMoves();
-            return isMutant(currentx, currenty, numberLetters)
-        }
-        else {
-            return false;
-        }
-    }
-    catch (e) {
-        console.log(e);
-        return new Error(e);
-    }
-
-}
-
+// post mutant
 exports.mutant = async function (req, res) {
     try{
-        setVars();
         const { dna } = req.body;
-        matrix = createMatrix(dna);
-        var mutant = isMutant(currentx, currenty, numberLetters);
-        dnaMutant = {
-           id: uuid(),
-           dna: dna,
-           mutant
-        }
+        var mutant = new Mutant(uuid(), dna);
+        var isMutant = mutant.isMutant(mutant.currentx, mutant.currenty, mutant.numberLetters);
+        //Config item dynamoDB
         const params = {
             TableName: TABLE_NAME,
-            Item: dnaMutant,
-        };
-        console.log(params);
+            Item: { dna: mutant.dna, mutant: isMutant, id: mutant.id },
+        }
         await dynamoclient.put(params).promise();
-        if(mutant){
+        if(isMutant){
             res.status(200).json({
                 message: 'Is Mutant!'
             })
@@ -152,6 +31,7 @@ exports.mutant = async function (req, res) {
     }
 }
 
+// get stats
 exports.stats = async function (req,res){
     try{
         const params = {
---
+++ src/components/mutant/mutant.model.js
@@ -0,0 +1,129 @@
+
+
+class Mutant {
+    constructor(id, dna) {
+        this.id = id;
+        this.dna = dna;
+        //if is true is mutant, if is false is human
+        this.flag = false;
+        //constants
+        this.numberLetters = 4;
+        this.RIGTH = 'RTGTH';
+        this.DOWN = 'DOWN';
+        this.OBLIQUE_LEFT = 'OBLIQUE_LEFT';
+        this.OBLIQUE_RIGTH = 'OBLIQUE_RIGTH';
+        //build matrix
+        this.createMatrix(dna);
+        //vars
+        this.setVars();
+        this.setMoves();
+    }
+
+
+    //Create a matrix of objects than contains the letter and its position in x and y.
+    createMatrix(dna) {
+        this.matrix = [];
+        dna.forEach((gen, x) => {
+            var arreglo = gen.split('').map((gen, y) => { return { id: gen, x, y } })
+            this.matrix[x] = arreglo;
+        })
+        return this.matrix;
+    }
+
+    setMoves() {
+        this.moveRigth = true;
+        this.moveDown = true;
+        this.moveObliqueRigth = true;
+        this.moveObliqueLefth = true;
+    }
+
+    setVars() {
+        this.currentx = 0;
+        this.currenty = 0;
+        this.numberGenesMutants = 0;
+    }
+
+    validateMoves(direction) {
+        direction === this.RIGTH ? this.moveRigth = false : direction === this.DOWN ? this.moveDown = false : direction === this.OBLIQUE_RIGTH ? this.moveObliqueRigth = false : this.moveObliqueLeft = false;
+    }
+
+    validationNextLetter(gen, nextGen, numberLetter, direction) {
+        var validationDirection = direction === this.RIGTH ? this.moveRigth : direction === this.DOWN ? this.moveDown : direction === this.OBLIQUE_RIGTH ? this.moveObliqueRigth : this.moveObliqueLeft;
+        var x= direction === this.RIGTH ? gen.x : direction === this.DOWN ? gen.x + 1 : direction === this.OBLIQUE_RIGTH ? gen.x + 1 : gen.x - 1;
+        var y= direction === this.RIGTH ? gen.y + 1 : direction === this.DOWN ? gen.y : direction === this.OBLIQUE_RIGTH ? gen.y + 1 : gen.y + 1;
+        if (gen.id === nextGen.id) {
+            if (numberLetter === 2) {
+                this.numberGenesMutants++;
+                if (this.numberGenesMutants > 1) {
+                    this.flag=true;
+                    return true;
+                }
+                this.validateMoves(direction);
+                return this.isMutant(this.currentx, this.currenty, this.numberLetters)
+            }
+            return this.isMutant(x, y, numberLetter - 1)
+        }
+        else if (validationDirection && gen.id !== nextGen.id) {
+            this.validateMoves(direction);
+            return this.isMutant(this.currentx, this.currenty, this.numberLetters)
+        }
+    }
+
+
+    isMutant(x, y, numberLetter) {
+
+        try {
+            var gen = this.matrix[x][y];
+            var genRigth = this.matrix[x][y + 1];
+            var genDown = this.matrix[x + 1] ? this.matrix[x + 1][y] : undefined;
+            var genObliqueRigth = this.matrix[x + 1] ? this.matrix[x + 1][y + 1] : undefined;
+            var genObliqueLeft = this.matrix[x + 1] ? this.matrix[x + 1][y - 1] : undefined;
+    
+            //Validation Rigth
+            if (gen.y + numberLetter <= this.matrix.length && this.moveRigth) {
+                return this.validationNextLetter(gen, genRigth, numberLetter, this.RIGTH)
+            } else {
+                this.moveRigth = false;
+            }
+            //Validation down
+            if (gen.x + numberLetter <= this.matrix.length && this.moveDown) {
+                return this.validationNextLetter(gen, genDown, numberLetter, this.DOWN)
+            } else {
+                this.moveDown = false;
+            }
+            //Validation obliqueRigth
+            if (gen.y + numberLetter <= this.matrix.length && gen.x + numberLetter <= this.matrix.length && this.moveObliqueRigth) {
+                return this.validationNextLetter(gen, genObliqueRigth, numberLetter, this.OBLIQUE_RIGTH)
+            } else {
+                this.moveObliqueRigth = false;
+            }
+            //Validation obliqueLeft
+            if (gen.y + numberLetter <= this.matrix.length && gen.x - numberLetter >= 0 && this.moveObliqueLeft) {
+                return this.validationNextLetter(gen, genObliqueLeft, numberLetter, this.OBLIQUE_LEFT)
+            } else {
+                this.moveObliqueLeft = false;
+            }
+    
+            if (this.currenty + 1 < this.matrix.length) {
+                this.currenty++;
+                this.setMoves();
+                return this.isMutant(this.currentx, this.currenty, this.numberLetters)
+            }
+            else if (this.currentx + 1 < this.matrix.length) {
+                this.currenty = 0;
+                this.currentx++;
+                this.setMoves();
+                return this.isMutant(this.currentx, this.currenty, this.numberLetters)
+            }
+            else {
+                return false;
+            }
+        }
+        catch (e) {
+            return new Error(e);
+        }
+    
+    }
+}
+
+module.exports = Mutant;
